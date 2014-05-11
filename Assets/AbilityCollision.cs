cb6db5892a1560c1cd85aec6869876ea83f40748
Nathan <nathan.p.lin@gmail.com>
2014-05-11 16:29:46 -0700

    Nathan's Changes - Use blockgame - Nathan, added ability blocks

---
+++ Assets/AbilityCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilityCollision : MonoBehaviour {
+
+	GameObject AbilityGen;
+	// Use this for initialization
+	void Start () {
+		AbilityGen = GameObject.Find("Ability Generator");
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.collider.name == "First Person Controller" || collision.collider.name == "Cube(Clone)") {
+			AbilityGen.GetComponent<AbilityGenerator>().removeAbility (gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward);
+	}
+}
---
+++ Assets/AbilityGenerator.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilityGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject Box;
+	GameObject[,] Boxes = new GameObject[5,5];
+	int[,] checkFull = new int[5, 5];
+	int count = 0;
+
+	void Start () {
+		for(int i = 0; i < 5; i++){
+			for(int j = 0; j < 5; j++){
+				checkFull[i,j] = 0;
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(count < 4){
+			create_ability();
+			count++;
+		}
+	}
+	void create_ability(){
+
+		int x = Random.Range (0,5);
+		int z = Random.Range (0,5);
+
+		while(checkFull[x,z] == 1){
+			x = Random.Range (0,5);
+			z = Random.Range (0,5);
+		}
+		Vector3 pos = new Vector3 (-10 + 5 * x, 1, -10 + 5 * z);
+
+		Boxes[x, z] = (GameObject)Instantiate (Box, pos, Box.transform.rotation);
+		checkFull [x, z] = 1;
+	}
+
+	public void removeAbility(GameObject ability) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (Boxes[i,j] == ability) {
+					checkFull[i,j] = 0;
+					//DestroyImmediate(ability);
+					Destroy(ability);
+					count--;
+				}
+			}
+		}
+	}
+
+}
